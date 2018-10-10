718b6ab7593af8456d3b556b28c06a0a7d01556c
chrislynch42 <chrislynch42@yahoo.com>
2018-10-10 12:10:52 -0400

    Successfule unit test DirSet

--- maven/src/test/java/com/codeexcursion/ant/types/DirSetTest.java
+++ maven/src/test/java/com/codeexcursion/ant/types/DirSetTest.java
@@ -4,51 +4,49 @@
  * and open the template in the editor.
  */
 package com.codeexcursion.ant.types;
+
 import com.codeexcursion.ant.TestHelper;
 import java.util.Objects;
+import java.util.stream.Collectors;
+
 import org.apache.tools.ant.Project;
+import org.apache.tools.ant.types.Resource;
 import org.junit.Test;
+import org.junit.Assert;
+import com.codeexcursion.ant.util.PathsUtil;
+import java.util.List;
+import java.io.File;
+
 /**
  *
  * @author chris
  */
 public class DirSetTest {
-  
+
   @Test
   public void testFindFiles() {
     Project project = new Project();
-    project.setName("Directory set unit test.");      
+    project.setName("Directory set unit test.");
     DirSet dirSet = new DirSet(project);
-      
-    String sourceDir =  TestHelper.TREE_DIR;
-    
+
+    String sourceDir = TestHelper.TREE_DIR;
+
     dirSet.setDir(sourceDir);
-    dirSet.setIncludes("**/*.txt");
-    
-    
-    System.out.println("xxxxxxxxxxxxxxxxxx");
-    System.out.println("xxxxxxxxxxxxxxxxxx");
-    System.out.println("xxxxxxxxxxxxxxxxxx");
-    System.out.println("xxxxxxxxxxxxxxxxxx");
-    System.out.println("xxxxxxxxxxxxxxxxxx");
-    System.out.println("xxxxxxxxxxxxxxxxxx");
-    dirSet.getAntType().stream().filter(Objects::nonNull).forEach((item) -> System.out.println(item.getName()));
-    System.out.println("yyyyyyyyyyyyyyyyyyy");
-    System.out.println("yyyyyyyyyyyyyyyyyyy");
-    System.out.println("yyyyyyyyyyyyyyyyyyy");
-    System.out.println("yyyyyyyyyyyyyyyyyyy");
-    System.out.println("yyyyyyyyyyyyyyyyyyy");
-    System.out.println("yyyyyyyyyyyyyyyyyyy");
-    
-   System.out.println(dirSet.getAntType().toString());
-    System.out.println("zzzzzzzzzzzzzzzzzzzzzzz");
-    System.out.println("zzzzzzzzzzzzzzzzzzzzzzz");
-    System.out.println("zzzzzzzzzzzzzzzzzzzzzzz");
-    System.out.println("zzzzzzzzzzzzzzzzzzzzzzz");
-    System.out.println("zzzzzzzzzzzzzzzzzzzzzzz");
+    dirSet.setIncludes("**/*");
+
+    System.out.println(PathsUtil.getFile(sourceDir).getAbsolutePath());
+
+    List<String> directories = dirSet.getAntType().stream()
+    .filter(Objects::nonNull)
+    .map(Resource::getName)
+    .peek(System.out::println)
+    .collect(Collectors.toList());
+
+    Assert.assertTrue("Directory list should contain test", directories.contains("test"));
+    Assert.assertTrue("Directory list should contain test/subTest", directories.contains("test/subTest"));
+
+    System.out.println(dirSet.getAntType().toString());
 
   }
 
-  
-  
 }
